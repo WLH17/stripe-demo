7b96f69f3a901f225e408ad3266dd2d5a7b42075
mattcbodily <mattcbodily@gmail.com>
2020-04-22 14:22:59 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import stripe from './stripe';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  onToken = async(token) => {
+    token.card = void 0;
+
+    await axios.post('/api/payment', {token, amount: 100})
+          .then(() => {
+            alert('Payment Submitted')
+          })
+          .catch(err => console.log(err))
+  }
+
   render(){
     return (
       <div className="App">
-  
+        <StripeCheckout 
+          label='Proceed to Checkout'
+          token={this.onToken}
+          stripeKey={stripe.publicKey}
+          amount={100}
+          //shippingAddress={true}
+          //billingAddress={true}
+          />
       </div>
     );
   }
